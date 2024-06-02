752684ca1943f4c36b425c4f0453d6a88735c894
DiegoFernando01 <diegofvictoria2013@gmail.com>
2024-06-01 23:16:48 -0500

    Adds init login view navigation to sign-up and sign-in login view

--- app/src/main/java/com/example/filmspot/view/fragment/InitFragment.kt
+++ app/src/main/java/com/example/filmspot/view/fragment/InitFragment.kt
@@ -5,22 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.filmspot.R
+import com.example.filmspot.databinding.FragmentInitBinding
 
 class InitFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+    private var _binding: FragmentInitBinding? = null
+    private val binding get() = _binding!!
 
-        }
-    }
-
-    override fun onCreateView(
+    override fun onCreateView( // Operaciones sobre la vista durante su creación
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_init, container, false)
+    }
+
+    override fun onViewCreated( // Operaciones sobre la vista después su creación
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        controllers()
+    }
+
+    private fun controllers() {
+        binding.btSignin.setOnClickListener() {
+            findNavController().navigate(R.id.action_InitFragment_to_SignInFragment)
+        }
+        binding.btSignup.setOnClickListener() {
+            findNavController().navigate(R.id.action_InitFragment_to_SignUpFragment)
+        }
     }
 
 }
