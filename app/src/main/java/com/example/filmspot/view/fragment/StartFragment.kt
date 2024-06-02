843c95a9284162ac795ea0e160cfdb33315c5328
DiegoFernando01 <diegofvictoria2013@gmail.com>
2024-06-01 23:08:17 -0500

    Adds start login view navigation to init login view

--- app/src/main/java/com/example/filmspot/view/fragment/StartFragment.kt
+++ app/src/main/java/com/example/filmspot/view/fragment/StartFragment.kt
@@ -5,22 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.filmspot.R
+import com.example.filmspot.databinding.FragmentStartBinding
 
-class StartFragment : Fragment() {
+class StartFragment : Fragment() { // Fragmento inicial
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView( // Operaciones sobre la vista durante su creación
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated( // Operaciones sobre la vista después su creación
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        controllers()
+    }
+
+    private fun controllers() {
+        binding.imgbtContinue.setOnClickListener() {
+            startLogin()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_start, container, false)
+    private fun startLogin() { // Función para ir del start login al init login
+        findNavController().navigate(R.id.action_StartFragment_to_InitFragment)
     }
 
 }
